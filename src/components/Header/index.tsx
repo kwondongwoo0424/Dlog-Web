845e7301674985dad6a7468fd6ccdd538273f45e
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-27 21:43:13 +0900

    init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./style"
+
+const Header = () => {
+  return (
+    <div>
+      <p>header</p>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/PostCard/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./style";
+
+const PostCard = () => {
+  return (
+    <div>
+      <p>PostCard</p>
+    </div>
+  );
+};
+
+export default PostCard;
