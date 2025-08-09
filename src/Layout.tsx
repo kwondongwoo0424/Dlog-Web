1842e3e56e42c1bdace0a2916e017fb645768ec8
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-08-09 23:16:15 +0900

    Footer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Header />
       {children}
       {isSearchOpen && <input type="text" />}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+const Footer = () => {
+    return (
+        <p>Dlog</p>
+    )
+}
+
+export default Footer;
